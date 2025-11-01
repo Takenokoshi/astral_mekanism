9ff20f9ed9c5a43c3000b6111a6f485b15d381c8
Takenokoshi <takenokoshichannel@gmail.com>
2025-11-01 21:54:11 +0900

    <commit>

--- src/main/java/astral_mekanism/recipes/inputRecipeCache/ArrayItemInputCache.java
+++ src/main/java/astral_mekanism/recipes/inputRecipeCache/ArrayItemInputCache.java
@@ -3,13 +3,11 @@ package astral_mekanism.recipes.inputRecipeCache;
 import astral_mekanism.recipes.ingredient.ArrayItemStackIngredient;
 import astral_mekanism.util.ArrayItemStackUtils;
 import mekanism.api.recipes.MekanismRecipe;
-import mekanism.common.lib.inventory.HashedItem;
-import mekanism.common.recipe.lookup.cache.type.NBTSensitiveInputCache;
-import net.minecraft.world.item.Item;
+import mekanism.common.recipe.lookup.cache.type.BaseInputCache;
 import net.minecraft.world.item.ItemStack;
 
 public class ArrayItemInputCache<RECIPE extends MekanismRecipe>
-        extends NBTSensitiveInputCache<Item[], HashedItem[], ItemStack[], ArrayItemStackIngredient, RECIPE> {
+        extends BaseInputCache<ItemStackArrayKey, ItemStack[], ArrayItemStackIngredient, RECIPE> {
 
     @Override
     public boolean isEmpty(ItemStack[] input) {
@@ -18,29 +16,16 @@ public class ArrayItemInputCache<RECIPE extends MekanismRecipe>
 
     @Override
     public boolean mapInputs(RECIPE recipe, ArrayItemStackIngredient inputIngredient) {
-        Item[] input = new Item[inputIngredient.ingredients.length];
-        for (int i = 0; i < input.length; i++) {
-            input[i] = inputIngredient.ingredients[i].getRepresentations().get(0).getItem();
+        ItemStack[] stacks = new ItemStack[inputIngredient.ingredients.length];
+        for (int i = 0; i < stacks.length; i++) {
+            stacks[i] = inputIngredient.ingredients[i].getRepresentations().get(0);
         }
-        addInputCache(input, recipe);
+        addInputCache(createKey(stacks), recipe);
         return false;
     }
 
     @Override
-    protected HashedItem[] createNbtKey(ItemStack[] input) {
-        HashedItem[] result = new HashedItem[input.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = HashedItem.raw(input[i]);
-        }
-        return result;
-    }
-
-    @Override
-    protected Item[] createKey(ItemStack[] input) {
-        Item[] result = new Item[input.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = input[i].getItem();
-        }
-        return result;
+    protected ItemStackArrayKey createKey(ItemStack[] input) {
+        return new ItemStackArrayKey(input);
     }
 }
