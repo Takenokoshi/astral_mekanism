6a3f4b3a80b6f2653ee54f496fe03258c1991d27
Takenokoshi <takenokoshichannel@gmail.com>
2025-12-26 16:48:02 +0900

    c

---
+++ src/datagen/main/java/astral_mekanism/DataGenerators.java
@@ -0,0 +1,23 @@
+import astral_mekanism.AstralMekanismID;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+@Mod.EventBusSubscriber(
+    modid = AstralMekanismID.MODID,
+    bus = Mod.EventBusSubscriber.Bus.MOD
+)
+public class DataGenerators {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+
+        DataGenerator gen = event.getGenerator();
+        PackOutput output = gen.getPackOutput();
+        ExistingFileHelper helper = event.getExistingFileHelper();
+
+        // provider は後で追加する
+    }
+}
