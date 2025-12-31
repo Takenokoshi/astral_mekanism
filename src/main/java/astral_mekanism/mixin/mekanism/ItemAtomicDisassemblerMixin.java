3bb350ff56fb0e25702451fae5a0d39fe506bca6
Takenokoshi <takenokoshichannel@gmail.com>
2025-12-31 09:44:39 +0900

    AtomicDisassembler

---
+++ src/main/java/astral_mekanism/mixin/mekanism/ItemAtomicDisassemblerMixin.java
@@ -0,0 +1,58 @@
+package astral_mekanism.mixin.mekanism;
+
+import java.util.Set;
+
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import mekanism.common.item.gear.ItemAtomicDisassembler;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+
+@Mixin(value = ItemAtomicDisassembler.class, remap = false)
+public class ItemAtomicDisassemblerMixin {
+
+    private static final Set<Enchantment> DISALLOWED = Set.of(
+            Enchantments.MENDING,
+            Enchantments.UNBREAKING,
+            Enchantments.BLOCK_EFFICIENCY,
+            Enchantments.SHARPNESS,
+            Enchantments.BANE_OF_ARTHROPODS,
+            Enchantments.SMITE,
+            Enchantments.FIRE_ASPECT,
+            Enchantments.SWEEPING_EDGE);
+
+    @Inject(method = "isEnchantable", at = @At("RETURN"), cancellable = true)
+    private void isEnchantableInject(@NotNull ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+        return;
+    }
+
+    @Inject(method = "isBookEnchantable", at = @At("RETURN"), cancellable = true)
+    private void isBookEnchantableInject(ItemStack stack, ItemStack book, CallbackInfoReturnable<Boolean> cir) {
+        boolean result = book.getAllEnchantments().keySet().stream()
+                .allMatch(enchantment -> ((ItemAtomicDisassembler) (Object) (this))
+                        .canApplyAtEnchantingTable(stack, enchantment));
+        cir.setReturnValue(result);
+        return;
+    }
+
+    @Inject(method = "canApplyAtEnchantingTable", at = @At("RETURN"), cancellable = true)
+    private void canApplyAtEnchantingTableInject(ItemStack stack, Enchantment enchantment,
+            CallbackInfoReturnable<Boolean> cir) {
+        if (DISALLOWED.contains(enchantment)) {
+            cir.setReturnValue(false);
+            return;
+        } else {
+            boolean allowed = enchantment.category.canEnchant(Items.NETHERITE_PICKAXE)
+                    || enchantment.category.canEnchant(Items.NETHERITE_SWORD);
+            cir.setReturnValue(allowed);
+            return;
+        }
+    }
+}
