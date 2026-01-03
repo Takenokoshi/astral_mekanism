c9478db3a5041f9e5724131568f9e28ecced2d8f
Takenokoshi <takenokoshichannel@gmail.com>
2026-01-03 16:29:18 +0900

    c

--- src/main/java/astral_mekanism/mixin/mekanism/TileEntityMetallurgicInfuserMixin.java
+++ src/main/java/astral_mekanism/mixin/mekanism/TileEntityMetallurgicInfuserMixin.java
@@ -1,16 +1,44 @@
 package astral_mekanism.mixin.mekanism;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import mekanism.api.AutomationType;
+import mekanism.api.IContentsListener;
+import mekanism.api.chemical.ChemicalTankBuilder;
+import mekanism.api.chemical.infuse.IInfusionTank;
+import mekanism.api.chemical.infuse.InfuseType;
+import mekanism.api.chemical.infuse.InfusionStack;
 import mekanism.common.tile.machine.TileEntityMetallurgicInfuser;
 
 @Mixin(value = TileEntityMetallurgicInfuser.class, remap = false)
 public abstract class TileEntityMetallurgicInfuserMixin {
 
-    @ModifyConstant(method = "<getInitialInfusionTanks>", constant = @Constant(longValue = 1_000L))
-    private long astral_mekanism$increaseMaxInfuse(long original) {
-        return 1_000_000L;
+    @Redirect(
+        method = "getInitialInfusionTanks",
+        at = @At(
+            value = "INVOKE",
+            target = "Lmekanism/common/capabilities/chemical/ChemicalTankBuilder;create(JLjava/util/function/BiPredicate;Ljava/util/function/BiPredicate;Ljava.util.function.Predicate;Lmekanism.api.IContentsListener;)Lmekanism/common/capabilities/chemical/infuse/InfusionTank;"
+        )
+    )
+    private IInfusionTank astral_mekanism$redirectCreate(
+        ChemicalTankBuilder<InfuseType, InfusionStack, IInfusionTank> self,
+        long capacity,
+        BiPredicate<InfuseType, AutomationType> alwaysTrue,
+        BiPredicate<InfuseType, AutomationType> canInsert,
+        Predicate<InfuseType> canExtract,
+        IContentsListener listener
+    ) {
+        return self.create(
+            1_000_000L,
+            alwaysTrue,
+            canInsert,
+            canExtract,
+            listener
+        );
     }
 }
