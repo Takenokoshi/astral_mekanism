5f34d86fa3da0c275d7d87ff0875807d20c3cf06
Takenokoshi <takenokoshichannel@gmail.com>
2026-01-07 22:49:23 +0900

    c

--- src/main/java/astral_mekanism/mixin/mekanism/recipe/cache/CachedRecipeMixin.java
+++ src/main/java/astral_mekanism/mixin/mekanism/recipe/cache/CachedRecipeMixin.java
@@ -1,8 +1,6 @@
 package astral_mekanism.mixin.mekanism.recipe.cache;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-
 import astral_mekanism.generalrecipe.cachedrecipe.ICachedRecipe;
 import mekanism.api.recipes.MekanismRecipe;
 import mekanism.api.recipes.cache.CachedRecipe;
@@ -10,20 +8,4 @@ import mekanism.api.recipes.cache.CachedRecipe;
 @Mixin(value = CachedRecipe.class, remap = false)
 public abstract class CachedRecipeMixin<RECIPE extends MekanismRecipe>
         implements ICachedRecipe<RECIPE> {
-
-    @Shadow
-    @Override
-    public abstract void loadSavedOperatingTicks(int operatingTicks);
-
-    @Shadow
-    @Override
-    public abstract void process();
-
-    @Shadow
-    @Override
-    public abstract boolean isInputValid();
-
-    @Shadow
-    @Override
-    public abstract RECIPE getRecipe();
 }
